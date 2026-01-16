81adb7ca19f95a5fcf3437b8013fec2f4b5dcc0c
Richard Kirsch <90441073+richard-kirsch@users.noreply.github.com>
2026-01-16 13:17:40 -0500

    Changed FlightSearchQuery data object to accept multiple origins, destinations and departure dates

--- data/FlightSearchQuery.py
+++ data/FlightSearchQuery.py
@@ -1,11 +1,29 @@
-from pydantic import BaseModel, Field, field_validator
+from typing import List
 from datetime import date
 
-from data.Airport import Airport
+from data import Airport
+
+from pydantic import BaseModel, Field
 
 
 class FlightSearchQuery(BaseModel):
-    origin: Airport = Field(...)
-    destination: str = Field(..., min_length=3, max_length=3, description="IATA code for destination airport")
-    departure_date: date = Field(..., description="Date of departure (YYYY-MM-DD)")
+    # Change origin to a list of Airport objects
+    origins: List[Airport] = Field(
+        ...,
+        min_length=1,
+        description="List of origin Airports"
+    )
 
+    # Change destination to a list of Airport objects
+    destinations: List[Airport] = Field(
+        ...,
+        min_length=1,
+        description="List of destination Airports"
+    )
+
+    # Change departure_date to a list of dates
+    departure_dates: List[date] = Field(
+        ...,
+        min_length=1,
+        description="List of dates of departure (YYYY-MM-DD)"
+    )
